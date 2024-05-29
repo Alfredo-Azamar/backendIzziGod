e7abf3e025a87ca4db2dc8d3f4f498b1df10c976
abner-lecona <abner_lecona@outlook.com>
2024-05-29 14:55:41 -0600

    Iniciando el login

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,40 @@
 import Server from './provider/Server';
-import { PORT, NODE_ENV } from './config'; // Si solo tiene el nombre de la carpeta, abre el index
+import {PORT,NODE_ENV} from './config'; // Si solo tiene el nombre de la carpeta, abre el index
 import express from 'express';
 import cors from 'cors';
 import EmpleadoController from './controllers/EmpleadoController';
 import ClienteController from './controllers/ClienteController';
 import LlamadaController from './controllers/LlamadaController';
 import ReporteController from './controllers/ReporteController';
-import SNSController from './controllers/SNSController';
+import AuthenticationController from './controllers/AuthenticationController';
 
 const server = new Server({
-    port: PORT,
-    env: NODE_ENV,
-    middlewares: [
+    port:PORT,
+    env:NODE_ENV,
+    middlewares:[
         express.json(),
-        express.urlencoded({ extended: true }),
+        express.urlencoded({extended:true}),
         cors()
     ],
-    controllers: [
+    controllers:[
         EmpleadoController.instance,
         ClienteController.instance,
         LlamadaController.instance,
         ReporteController.instance,
-        SNSController.instance
+        AuthenticationController.instance
     ]
 });
+
+
+//Extendiendo la interfaz Request de Express para poder acceder a los datos del usuario
+declare global {
+    namespace Express {
+        interface Request {
+            user: string;
+            token: string;
+        }
+    }
+}
+
 
 server.init();
