240a8839d72e53e4ffcbed47aff75a23783f1213
Alfredo-Azamar <a01798100@tec.mx>
2024-06-17 13:29:30 -0600

    Changes to LlamadaController

--- src/models/SolucionBaseModel.ts
+++ src/models/SolucionBaseModel.ts
@@ -1,57 +1,68 @@
-import { Model, Sequelize } from "sequelize";
+// Authors:
+// * Alfredo Azamar López - A01798100
 
-enum AsuntoEnum{
-    Ventas = "ventas",
-    Internet = "internet",
-    Telefonia = "telefonia",
-    Television = "television",
-    Soporte = "soporte",
-  }
+// {IMPORTS}
+import { Model } from "sequelize";
 
-interface SolucionBaseAttributes {
-    IdSolucion: number;
-    Nombre: string;
-    Asunto: AsuntoEnum;
+// Enum for the subject attribute
+enum AsuntoEnum {
+  Ventas = "ventas",
+  Internet = "internet",
+  Telefonia = "telefonia",
+  Television = "television",
+  Soporte = "soporte",
 }
 
-module.exports = (sequelize: any, DataTypes: any) => {
-    class SolucionBase
-        extends Model<SolucionBaseAttributes>
-        implements SolucionBaseAttributes
-    {
-        public IdSolucion!: number;
-        public Nombre!: string;
-        public Asunto!: AsuntoEnum;
+// Interface for the SolucionBase model
+interface SolucionBaseAttributes {
+  IdSolucion: number;
+  Nombre: string;
+  Asunto: AsuntoEnum;
+}
 
-        static associate(models: any) {
-            SolucionBase.hasMany(models.Pasos, {
-                foreignKey: "IdSolucion",
-                as: "Pasos",
-            });
-        }
+// Define the SolucionBase model, for the Sequelize ORM, representing a solution in the database
+module.exports = (sequelize: any, DataTypes: any) => {
+  class SolucionBase
+    extends Model<SolucionBaseAttributes>
+    implements SolucionBaseAttributes
+  {
+    // Attributes are enforced by the SolucionBaseAttributes interface
+    public IdSolucion!: number;
+    public Nombre!: string;
+    public Asunto!: AsuntoEnum;
+
+    // Associates the SolucionBase model with other models
+    static associate(models: any) {
+      SolucionBase.hasMany(models.Pasos, {
+        foreignKey: "IdSolucion",
+        as: "Pasos",
+      });
     }
-    SolucionBase.init(
-        {
-            IdSolucion: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                primaryKey: true,
-                autoIncrement: true,
-            },
-            Nombre: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            Asunto: {
-                type: DataTypes.ENUM,
-                values: Object.values(AsuntoEnum),
-                allowNull: false,
-            },
-        },
-        {
-            sequelize,
-            modelName: "SolucionBase",
-        }
-    );
-    return SolucionBase;
+  }
+
+  // Initializes the SolucionBase model with its attributes and options
+  SolucionBase.init(
+    {
+      IdSolucion: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      Nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      Asunto: {
+        type: DataTypes.ENUM,
+        values: Object.values(AsuntoEnum),
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "SolucionBase", // Name of the model
+    }
+  );
+  return SolucionBase;
 };
